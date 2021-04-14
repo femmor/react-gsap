1074ec42f4835c91672035af93342233a585f55a
Emmanuel Egomson <fegomson@gmail.com>
2021-04-14 13:55:05 -0500

    Animate when react state changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import React, { useRef, useEffect } from "react"
-import {gsap} from "gsap"
+import React, { useRef, useState, useEffect } from "react"
+import { gsap } from "gsap"
 import logo from "./images/gsap.svg"
 import reactLogo from "./images/react-logo.svg"
 
 function App() {
-  const logoName = "GSAP Logo"
+  const [background, setBackground] = useState('#5a7d95');
 
   const headerRef = useRef(null)
 
+  const toggleBackground = () => {
+    const color = background !== '#5a7d95' ? '#5a7d95' : '#1b4943'
+    setBackground(color)
+  }
+
+  // Change backgroundround to new value
   useEffect(() => {
-    
-    gsap.from(headerRef.current, {
-      duration: 1, 
-      autoAlpha: 0, 
-      ease: 'none', 
-      delay: 1})
+    gsap.to(headerRef.current, 
+      {
+        duration: 1, 
+        backgroundColor: background, 
+        ease: 'none'
+      })
+  }, [background])
+
+  // Animate the headerRef
+  useEffect(() => {
+    gsap.from(headerRef.current, 
+      {
+        duration: 1, 
+        autoAlpha: 0, 
+        ease: 'none', 
+        delay: 1
+      })
   }, [])
 
   return (
@@ -22,8 +39,9 @@ function App() {
       <header className="App-header" ref={headerRef}>
         <div className="logos">
           <img className="react-logo" src={reactLogo} alt="react logo" />
-          <img className="logo" src={logo} alt={logoName} />
+          <img className="logo" src={logo} alt="gsap logo" />
         </div>
+        <button onClick={() => toggleBackground()}>Toggle background</button>
         <div className="logo-sub">
           <p>Scroll down to see sections being revealed by ScrollTrigger</p>
         </div>
