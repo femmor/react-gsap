c1f565b445e8a04f4560880e63c79e24a5a7397d
Emmanuel Egomson <fegomson@gmail.com>
2021-04-14 13:32:28 -0500

    Setup GSAP and target elements using React useRef

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react"
+import {gsap} from "gsap"
+import logo from "./images/gsap.svg"
+import reactLogo from "./images/react-logo.svg"
 
 function App() {
+  const logoName = "GSAP Logo"
+
+  const headerRef = useRef(null)
+
+  useEffect(() => {
+    
+    gsap.from(headerRef.current, {
+      duration: 1, 
+      autoAlpha: 0, 
+      ease: 'none', 
+      delay: 1})
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" ref={headerRef}>
+        <div className="logos">
+          <img className="react-logo" src={reactLogo} alt="react logo" />
+          <img className="logo" src={logo} alt={logoName} />
+        </div>
+        <div className="logo-sub">
+          <p>Scroll down to see sections being revealed by ScrollTrigger</p>
+        </div>
       </header>
+      <main className="App-main"> 
+        <div className="App-section">
+          <h1>Lorem ipsum dolor sit.</h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis, porro?</p>
+        </div>
+      </main>
     </div>
   );
 }
